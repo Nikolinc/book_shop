42bc688986b108f11f2ba4a591ca61833cf8415a
Tsigulsky <tsigulskynikita@gmail.com>
2022-03-08 21:08:01 +0500

    moved to react

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from '../pages/home/index';
+import '../style/global.scss';
+
+function App() {
+  return (
+    <HomePage/>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/App.tsx';
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
