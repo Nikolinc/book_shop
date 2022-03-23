93286b78c1f78a1ab6467631c5b80cc53885c59f
Tsigulsky <tsigulskynikita@gmail.com>
2022-03-24 00:20:10 +0500

    getting products from api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import {useEffect,useState} from 'react';
+import axios from 'axios';
 import Header from '../pages/headers/index';
 import Product from '../pages/Product/index';
 import '../style/global.scss';
+
+
 
 interface Piece{
   image:string;
@@ -12,180 +15,47 @@ interface Piece{
  
 }
 
-const productArray : Piece[] = [];
-
-productArray.push({image:"https://img3.labirint.ru/rc/a63fd99361bfb913cf732d979d8b10ff/220x340/books75/747735/cover.jpg?1589469910",
-name:"Берсерк том 1 ", 
-auter:"Хуй Знает Кто Младший",
-price:"1990р",key:"1"})
-
-productArray.push({image:"https://img-gorod.ru/28/966/2896636_detail.jpg",
-name:"Невеста Ноября", 
-auter:"Арден Л.",
-price:"518p",key:"2"})
-
-
-productArray.push({image:"https://img-gorod.ru/29/023/2902301_detail.jpg",
-name:"Живи и давай жить другим", 
-auter:"Грун Х.",
-price:"629p",key:"3"})
-
-productArray.push({image:"https://img-gorod.ru/28/922/2892294_detail.jpg",
-name:"Элизиум", 
-auter:"Сакавич Н.",
-price:"458p",key:"4"})
-
-productArray.push({image:"https://img-gorod.ru/29/025/2902568_detail.jpg",
-name:"Танец пылающего моря (#2)", 
-auter:"Меллоу Э.Дж.",
-price:"644p",key:"5"})
-
-productArray.push({image:"https://img-gorod.ru/29/025/2902568_detail.jpg",
-name:"Я - бездна", 
-auter:"Карризи Д.",
-price:"536p",key:"6"})
-
-productArray.push({image:"https://img-gorod.ru/29/042/2904206_detail.jpg",
-name:"Маркетинг 5.0. Технологии ...", 
-auter:"Котлер Ф.",
-price:"964р",key:"7"})
-
-productArray.push({image:"https://img-gorod.ru/22/216/2221617_detail.jpg",
-name:"Дом, в котором…", 
-auter:"Петросян М.",
-price:"776р",key:"8"})
-
-productArray.push({image:"https://img-gorod.ru/28/634/2863449_detail.jpg",
-name:"Пост", 
-auter:"Глуховский Д.",
-price:"904р",key:"9"})
-
-productArray.push({image:"https://img-gorod.ru/28/424/2842415_detail.jpg",
-name:"Доктор Гарин", 
-auter:"Сорокин В.",
-price:"804р",key:"10"})
-
-
-productArray.push({image:"https://img3.labirint.ru/rc/a63fd99361bfb913cf732d979d8b10ff/220x340/books75/747735/cover.jpg?1589469910",
-name:"Берсерк том 1 ", 
-auter:"Хуй Знает Кто Младший",
-price:"1990р",key:"1"})
-
-productArray.push({image:"https://img-gorod.ru/28/966/2896636_detail.jpg",
-name:"Невеста Ноября", 
-auter:"Арден Л.",
-price:"518p",key:"2"})
-
-
-productArray.push({image:"https://img-gorod.ru/29/023/2902301_detail.jpg",
-name:"Живи и давай жить другим", 
-auter:"Грун Х.",
-price:"629p",key:"3"})
-
-productArray.push({image:"https://img-gorod.ru/28/922/2892294_detail.jpg",
-name:"Элизиум", 
-auter:"Сакавич Н.",
-price:"458p",key:"4"})
-
-productArray.push({image:"https://img-gorod.ru/29/025/2902568_detail.jpg",
-name:"Танец пылающего моря (#2)", 
-auter:"Меллоу Э.Дж.",
-price:"644p",key:"5"})
-
-productArray.push({image:"https://img-gorod.ru/29/025/2902568_detail.jpg",
-name:"Я - бездна", 
-auter:"Карризи Д.",
-price:"536p",key:"6"})
-
-productArray.push({image:"https://img-gorod.ru/29/042/2904206_detail.jpg",
-name:"Маркетинг 5.0. Технологии ...", 
-auter:"Котлер Ф.",
-price:"964р",key:"7"})
-
-productArray.push({image:"https://img-gorod.ru/22/216/2221617_detail.jpg",
-name:"Дом, в котором…", 
-auter:"Петросян М.",
-price:"776р",key:"8"})
-
-productArray.push({image:"https://img-gorod.ru/28/634/2863449_detail.jpg",
-name:"Пост", 
-auter:"Глуховский Д.",
-price:"904р",key:"9"})
-
-productArray.push({image:"https://img-gorod.ru/28/424/2842415_detail.jpg",
-name:"Доктор Гарин", 
-auter:"Сорокин В.",
-price:"804р",key:"10"})
-
-productArray.push({image:"https://img3.labirint.ru/rc/a63fd99361bfb913cf732d979d8b10ff/220x340/books75/747735/cover.jpg?1589469910",
-name:"Берсерк том 1 ", 
-auter:"Хуй Знает Кто Младший",
-price:"1990р",key:"1"})
-
-productArray.push({image:"https://img-gorod.ru/28/966/2896636_detail.jpg",
-name:"Невеста Ноября", 
-auter:"Арден Л.",
-price:"518p",key:"2"})
-
-
-productArray.push({image:"https://img-gorod.ru/29/023/2902301_detail.jpg",
-name:"Живи и давай жить другим", 
-auter:"Грун Х.",
-price:"629p",key:"3"})
-
-productArray.push({image:"https://img-gorod.ru/28/922/2892294_detail.jpg",
-name:"Элизиум", 
-auter:"Сакавич Н.",
-price:"458p",key:"4"})
-
-productArray.push({image:"https://img-gorod.ru/29/025/2902568_detail.jpg",
-name:"Танец пылающего моря (#2)", 
-auter:"Меллоу Э.Дж.",
-price:"644p",key:"5"})
-
-productArray.push({image:"https://img-gorod.ru/29/025/2902568_detail.jpg",
-name:"Я - бездна", 
-auter:"Карризи Д.",
-price:"536p",key:"6"})
-
-productArray.push({image:"https://img-gorod.ru/29/042/2904206_detail.jpg",
-name:"Маркетинг 5.0. Технологии ...", 
-auter:"Котлер Ф.",
-price:"964р",key:"7"})
-
-productArray.push({image:"https://img-gorod.ru/22/216/2221617_detail.jpg",
-name:"Дом, в котором…", 
-auter:"Петросян М.",
-price:"776р",key:"8"})
-
-productArray.push({image:"https://img-gorod.ru/28/634/2863449_detail.jpg",
-name:"Пост", 
-auter:"Глуховский Д.",
-price:"904р",key:"9"})
-
-productArray.push({image:"https://img-gorod.ru/28/424/2842415_detail.jpg",
-name:"Доктор Гарин", 
-auter:"Сорокин В.",
-price:"804р",key:"10"})
-
 
 function App() {
 
+  const [data, setData] = useState<Piece[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:3500/product',
+        );
+    setData(result.data)
+      };
+      fetchData();
+      setLoading(false);
+  },[]);
+
+  if (loading){
+    return <div>loading</div>
+  }
+
   return (
     <><Header />
-    <Position/></>
+    <Position product = {data}/></>
         
   )
 }
 
-function Position(){
-  return(<div className='container'>
-    {productArray.map(pos => (<Product 
+
+const Position = ({product:price})=>{
+  console.log(product);
+  return(
+    <div className='container'>
+      {product.map(pos =>(<Product 
       image = {pos.image}
       name = {pos.name}
       auter = {pos.auter}
       price = {pos.price}
+
       />))}</div>
+      
   )
 }
 
