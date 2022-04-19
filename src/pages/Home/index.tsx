8ec0aaaa54d7b17e1f566305be4936e14d411e45
Nikolinc <tsigulskynikita@gmail.com>
2022-04-19 17:55:05 +0500

    add page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,8 +3,15 @@ import './style.scss'
 
 export default function HomePage(){
     return(
-        <div className="home">
-            
+        <div className="home">     
+
+           <div className="home_info">
+                <div className="bound"></div>
+                <div className="info_headers"><h1>Beware of  expensive books</h1><h3>We only have the best books</h3></div>
+                <div className="bound"></div>
+           </div>   
+           
+                  
         </div>
     )
 }
