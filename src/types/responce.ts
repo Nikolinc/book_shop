61d446be08047202d2b867a90555391e4bbd14bb
Nikolinc <tsigulskynikita@gmail.com>
2022-07-21 00:59:30 +0500

    add types for select server

---
+++ src/types/responce.ts
@@ -0,0 +1,20 @@
+export interface UserNewsType {
+  id: number
+  email: string
+  password: string
+  firstname: string
+  lastname: string
+}
+
+export interface UserNewsType {
+  name: string
+  author: string
+  price: string
+  currency: string
+  publisher: string
+  PublicationDate: Date
+  Language: string
+  PrintLength: string
+  cover: string
+  description: string
+}
