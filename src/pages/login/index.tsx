470f0c455a94608adfe630f57e5444f1abd2f68f
Nikolinc <tsigulskynikita@gmail.com>
2022-07-25 17:31:32 +0500

    add login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import './style.scss'
-export default function Login(){
+export default function Login() {
   return (
     <>
       <div className="login">
         <div className="lamp">
-          <img src="" alt="" />
+          <img
+            src="https://github.com/Nikolinc/book_shop/blob/book-search/src/assets/lamp.png?raw=true"
+            alt="lamp"
+          />
+        </div>
+
+        <div className="authorisation">
+          <div className="namePage">LOGIN</div>
+
+          <div className="passwordEntryField">
+            <input type="Text" placeholder="E-mail or login" />
+          </div>
+
+          <div className="passwordEntryField">
+            <input type="Password" placeholder="Password" />
+          </div>
+          <div className="reestablish">
+            Forgot your password?<a>Reestablish</a>
+          </div>
+          <div className="loginButton">
+            <button>
+              <div className="textButton">Login</div> 
+            </button>
+          </div>
         </div>
       </div>
     </>
